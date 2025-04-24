6da36f4b9f8c18a851a32059a7170e5617afe3f6
Kudzeri <kudzeri@gmail.com>
2025-04-24 17:13:15 +0500

    add recipe index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
-<script lang="ts">
+<script setup lang="ts">
+import RecipeCard from '~/components/RecipeCard.vue';
+
+
+const { data, error } = await useFetch('https://dummyjson.com/recipes?limit=24');
 </script>
 
 <template>
@@ -12,12 +16,21 @@
           <p class="text-xl lg:text-2xl mb-8 text-balance">
             Discover recipes helping you to find the easiest way to cook.
           </p>
-          <UButton to="#recipes" label="Browse Recipes" />
+          <button class="px-4 py-2 text-white self-start gold-bg rounded-md text-lg cursor-pointer">Browse
+            Recipes</button>
         </div>
         <div class="flex-1 order-1 lg:order-2">
           <NuxtImg sizes="xs:100vw sm:667px" src="/nuxt-course-hero.png" format="webp" densities="x1" alt="" />
         </div>
       </div>
+    </section>
+    <section id="recipes" class="py-20 container">
+      <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
+      <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes!</p>
+      <div v-if="!error" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+        <RecipeCard v-for="recipe in data?.recipes" :recipe="recipe" />
+      </div>
+      <p v-else class="text-xl">Opps, something went wrong. Please try again later</p>
     </section>
   </main>
 </template>
