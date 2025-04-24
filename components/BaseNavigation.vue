f94ddc63d836cad9946d4831221ea29a0d4324e7
Kudzeri <kudzeri@gmail.com>
2025-04-24 16:49:11 +0500

    created hero section

--- components/BaseNavigation.vue
+++ components/BaseNavigation.vue
@@ -8,14 +8,14 @@ import { NuxtLink, NuxtImg } from '../.nuxt/components';
   <header class="py-4 border-b">
     <nav class="flex container items-center">
       <NuxtLink to="/" class="flex gap-1 items-center">
-        <NuxtImg width="56px" src="icon-green.png" format="webp" alt="Logo"/>
+        <NuxtImg width="56px" src="icon-green.png" format="webp" alt="Logo" densities="x1" />
         <span class="text-3xl text-black">Nuxtcipes</span>
       </NuxtLink>
       <ul class="flex gap-6 ml-auto text-xl capitalize">
         <li>
           <NuxtLink class="nav-item" to="/">Home</NuxtLink>
         </li>
-        <li >
+        <li>
           <NuxtLink class="nav-item" to="/about">About</NuxtLink>
         </li>
       </ul>
